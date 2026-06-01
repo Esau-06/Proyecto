ad1a193082a2fbdaf2308bba63082d2e7c27b4d1
Esau <esaumontoya2007@gmail.com>
2026-05-31 19:36:01 -0500

    Correccion HU4

--- Principal.java
+++ Principal.java
@@ -1,7 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Principal {
 
@@ -10,30 +7,18 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         Metodos m = new Metodos();
 
-        LinkedList vector_ingenieros = new LinkedList();
-        LinkedList vector_disenadores = new LinkedList();
-        LinkedList vector_portatil = new LinkedList();
-        LinkedList vector_tableta = new LinkedList();
-
-        Queue cola_solicitudes = new LinkedList();
-        Stack pila_devoluciones = new Stack();
-
         int opcion = 0;
 
         while (opcion != 4) {
 
-            System.out.println("==============================================");
-            System.out.println("GESTION PRESTAMO EQUIPOS ELECTRONICOS");
-            System.out.println("SAN JUAN DE DIOS");
-            System.out.println("==============================================");
+            System.out.println("GESTION PRESTAMO EQUIPOS ELECTRONICOS SAN JUAN DE DIOS");
             System.out.println("1. Estudiantes de Ingenieria");
             System.out.println("2. Estudiantes de Diseno");
             System.out.println("3. Imprimir inventario total");
             System.out.println("4. Salir del programa");
-            System.out.println("Digite una opcion:");
+            System.out.println("Digite una opcion");
 
             opcion = sc.nextInt();
-            sc.nextLine();
 
             switch (opcion) {
 
@@ -43,35 +28,32 @@ public class Principal {
 
                     while (opcionIngenieria != 4) {
 
-                        System.out.println("==============================================");
                         System.out.println("MENU ESTUDIANTES DE INGENIERIA");
-                        System.out.println("==============================================");
                         System.out.println("0. Registrar prestamo");
                         System.out.println("1. Modificar prestamo");
                         System.out.println("2. Devolucion de equipo");
                         System.out.println("3. Buscar equipo");
                         System.out.println("4. Volver al menu principal");
-                        System.out.println("Digite una opcion:");
+                        System.out.println("Digite una opcion");
 
                         opcionIngenieria = sc.nextInt();
-                        sc.nextLine();
 
                         switch (opcionIngenieria) {
 
                             case 0:
-                                vector_ingenieros = m.RegistrarPrestamoIngenieria(vector_ingenieros, vector_portatil, cola_solicitudes, sc);
+                                m.RegistrarPrestamoIngenieria();
                                 break;
 
                             case 1:
-                                vector_ingenieros = m.ModificarPrestamoIngenieria(vector_ingenieros, sc);
+                                m.ModificarPrestamoIngenieria();
                                 break;
 
                             case 2:
-                                vector_ingenieros = m.DevolverEquipoIngenieria(vector_ingenieros, vector_portatil, pila_devoluciones, sc);
+                                m.DevolverEquipoIngenieria();
                                 break;
 
                             case 3:
-                                m.BuscarEquipoIngenieria(vector_ingenieros, sc);
+                                m.BuscarEquipoIngenieria();
                                 break;
 
                             case 4:
@@ -92,35 +74,32 @@ public class Principal {
 
                     while (opcionDiseno != 4) {
 
-                        System.out.println("==============================================");
                         System.out.println("MENU ESTUDIANTES DE DISENO");
-                        System.out.println("==============================================");
                         System.out.println("0. Registrar prestamo");
                         System.out.println("1. Modificar prestamo");
                         System.out.println("2. Devolucion de equipo");
                         System.out.println("3. Buscar equipo");
                         System.out.println("4. Volver al menu principal");
-                        System.out.println("Digite una opcion:");
+                        System.out.println("Digite una opcion");
 
                         opcionDiseno = sc.nextInt();
-                        sc.nextLine();
 
                         switch (opcionDiseno) {
 
                             case 0:
-                                vector_disenadores = m.RegistrarPrestamoDiseno(vector_disenadores, vector_tableta, cola_solicitudes, sc);
+                                m.RegistrarPrestamoDiseno();
                                 break;
 
                             case 1:
-                                vector_disenadores = m.ModificarPrestamoDiseno(vector_disenadores, sc);
+                                m.ModificarPrestamoDiseno();
                                 break;
 
                             case 2:
-                                vector_disenadores = m.DevolverEquipoDiseno(vector_disenadores, vector_tableta, pila_devoluciones, sc);
+                                m.DevolverEquipoDiseno();
                                 break;
 
                             case 3:
-                                m.BuscarEquipoDiseno(vector_disenadores, sc);
+                                m.BuscarEquipoDiseno();
                                 break;
 
                             case 4:
@@ -136,7 +115,7 @@ public class Principal {
                     break;
 
                 case 3:
-                    m.ImprimirInventarioTotal(vector_ingenieros, vector_disenadores, vector_portatil, vector_tableta);
+                    m.ImprimirInventarioTotal();
                     break;
 
                 case 4:
@@ -150,4 +129,3 @@ public class Principal {
         }
     }
 }
-
